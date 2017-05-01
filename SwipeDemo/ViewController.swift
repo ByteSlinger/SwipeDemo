70799b6b5f5abdfc9b4d30ffccbf45d0b8ccabf7
Michael S. Hearn <michael@byteslinger.net>
2017-05-01 15:54:49 -0700

    Apple IOS Xcode storyboard-ONLY demo of swipe gestures
    This demo shows how to navigate between multiple views with swipe
    gestures.  There is no custom Swift coding - it’s done completely with
    Xcode and storyboards.
    
    The not-so-obvious trick is to CTRL-Click on each swipe gesture after
    you drag them onto the View Controller nav bar, and then drag-Click the
    “Referencing OutLet Collections” plus sign to the View in each View
    Controller.  Simple to do if you know about it, but the swipes do not
    work without it.

---
+++ SwipeDemo/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  SwipeDemo
+//
+//  Created by Michael S. Hearn on 4/24/17.
+//  Copyright © 2017 ByteSlinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.up
+        self.view.addGestureRecognizer(swipeUp)
+ */
+    }
+
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                print("Swiped right")
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+            case UISwipeGestureRecognizerDirection.left:
+                print("Swiped left")
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+            default:
+                break
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue)
+    {
+        print("myUnwindAction");
+    }
+}
+
